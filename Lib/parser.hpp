421dc81e26b9031207cdff4df06522b5f23c53c0
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-05-20 19:13:28 +0300

    yylval

--- Lib/parser.hpp
+++ Lib/parser.hpp
@@ -10,57 +10,7 @@ using namespace output;
 extern SemanticTable semantic_tabl;
 extern int yylineno;
 
-class Node
-{
-public:
-    virtual int get_value()
-    {
-        return 0;
-    }
-    virtual TypeEnum get_type()
-    {
-        return TYPE_UNDEFINED;
-    }
-};
 
-class Num : public Node{
-public:
-   int value;
-    Num(int number){
-        value=number;
-    }
-};
-
-class String : public Node{
-public:
-    string value;
-    String(string name){
-        value=name;
-    }
-};
-
-class Type : public Node{
-public:
-    TypeEnum type;
-    Type(TypeEnum typeE){
-        type=typeE;
-    }
-};
-
-
-//class Variable: public Node{
-//public:
-//    bool is_func;
-//    int size;
-//    TypeEnum type;
-//    vector<TypeEnum> arguments_types;
-//    Variable(int number){
-//        value=number;
-//    }
-//};
-
-
-#define YYSTYPE Node*
 
 
 
---
+++ Lib/yystypes.hpp
@@ -0,0 +1,58 @@
+//
+// Created by Emil on 20/05/2020.
+//
+
+#ifndef FANC_SEMANTIC_ANALYZER_YYSTYPES_HPP
+#define FANC_SEMANTIC_ANALYZER_YYSTYPES_HPP
+class Node
+{
+public:
+    virtual int get_value()
+    {
+        return 0;
+    }
+    virtual TypeEnum get_type()
+    {
+        return TYPE_UNDEFINED;
+    }
+};
+
+class Num : public Node{
+public:
+    int value;
+    Num(int number){
+        value=number;
+    }
+};
+
+class String : public Node{
+public:
+    string value;
+    String(string name){
+        value=name;
+    }
+};
+
+class Type : public Node{
+public:
+    TypeEnum type;
+    Type(TypeEnum typeE){
+        type=typeE;
+    }
+};
+
+
+//class Variable: public Node{
+//public:
+//    bool is_func;
+//    int size;
+//    TypeEnum type;
+//    vector<TypeEnum> arguments_types;
+//    Variable(int number){
+//        value=number;
+//    }
+//};
+
+
+#define YYSTYPE Node*
+#endif //FANC_SEMANTIC_ANALYZER_YYSTYPES_HPP
