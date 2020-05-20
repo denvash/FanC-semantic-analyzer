a600a0eeb03a321db2ddb6714a19f586f32a291c
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-05-20 17:39:17 +0300

    yylval

--- Lib/parser.hpp
+++ Lib/parser.hpp
@@ -7,32 +7,13 @@
 using namespace std;
 using namespace output;
 
-#define YYSTYPE yystype
+
 
 extern SemanticTable semantic_table;
 extern int yylineno;
 
-class Node
-{
-public:
-  virtual int get_value()
-  {
-    return 0;
-  }
-  virtual TypeEnum get_type()
-  {
-    return TYPE_UNDEFINED;
-  }
-};
 
-struct yystype
-{
-  int i_value;
-  TypeEnum e_type;
-  variable_t type;
-  string *str_value;
-  vector<TypeEnum> arguments_types;
-  Node *node;
-};
+
+
 
 #endif /* PARSER_HPP */
